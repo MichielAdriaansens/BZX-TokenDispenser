66620fedb87aa33abbe99f23bc349540c764a342
MichielAdriaansens <m.adriaansens@gmail.com>
2022-11-21 00:36:01 +0100

    imported react

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,5 +19,8 @@ module.exports = {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: privateKey
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
